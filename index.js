ec534ccb3e36c3f9df4e342f11e085b46a5cc8ac
+ <Peleanr1989@gmail.com>
2021-11-22 07:49:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+
 // Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+victory.innerHTML = "<h1>Radu is the champion</h1>";
